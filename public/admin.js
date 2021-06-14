202887f6143f1c4ce3d54f5b7ac0fc9024c91e9c
Akhil Sai <akhil@kali.akhil>
2021-06-14 21:20:32 +0530

    got user name on dashboard

--- public/admin.js
+++ public/admin.js
@@ -1,3 +1,6 @@
 const token =document.cookie
 const decodedToken = JSON.parse(atob(token.split(".")[1]))
 const username = decodedToken.username
+
+const user = document.querySelector(".user")
+user.innerText = username;
